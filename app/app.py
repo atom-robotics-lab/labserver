e2ec01d1cd06ed879ece7f2443d3c71307ea7979
HRA <Hriday Aggarwal>
2023-03-02 02:21:48 +0530

    updated flask application

--- app/app.py
+++ app/app.py
@@ -1,19 +1,21 @@
+import multiprocessing
 from http.server import BaseHTTPRequestHandler, HTTPServer
 from pymongo import MongoClient
-from datetime import date,datetime
+from datetime import date, datetime
 import pytz
 from gui import GUI
 try:
     client = MongoClient(
-        "mongodb+srv://admin:root@cluster0.rsbrxww.mongodb.net/?retryWrites=true&w=majority")
+        "mongodb+srv://admin:root@cluster0.rsbrxww.mongodb.net/?retryWrites=true&w=majority", connect=False)
     print("Connected successfully!!!")
 except:
     print("Could not connect to MongoDB")
 db = client.test
-
+collection2 = db.cardinfo
 collection = db.test
 Request = None
 MyFile = None
+
 
 class RequestHandler_httpd(BaseHTTPRequestHandler):
     def do_GET(self):
@@ -26,27 +28,34 @@ class RequestHandler_httpd(BaseHTTPRequestHandler):
         self.wfile.write(messagetosend)
         Request = self.requestline
         Request = Request[5: int(len(Request)-9)]
-        print('Your request is:')
+        print('Your card number is:')
         print(Request)
-        now = datetime.now(pytz.timezone('Asia/Kolkata'))
-        d1 = now.strftime("%d/%m/%Y")
-        current_time = now.strftime("%H:%M:%S")
-        rec1 = { "Name": Request,"Time":current_time,"Date":d1}
-        collection.insert_one(rec1)
+        query = {"Card ID": Request}
+        result = collection2.find_one(query)
+        if result:
+            print("Atom Member")
+            now = datetime.now(pytz.timezone('Asia/Kolkata'))
+            d1 = now.strftime("%d/%m/%Y")
+            current_time = now.strftime("%H:%M:%S")
+            rec1 = {"Card ID": Request,
+                    "Name": result["Name"], "Time": current_time, "Date": d1}
+            collection.insert_one(rec1)
+        else:
+            print("Not an Atom Member")
+
         return
 
 
-
-import multiprocessing
-
-def function1():
+def function2():
     print('Starting server ...')
     server_address_httpd = ('', 8080)
     httpd = HTTPServer(server_address_httpd, RequestHandler_httpd)
     httpd.serve_forever()
 
-def function2():
+
+def function1():
     GUI()
+
 
 if __name__ == "__main__":
     process1 = multiprocessing.Process(target=function1)
