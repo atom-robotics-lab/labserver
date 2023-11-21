230bf668721a9ee2bb42b1471e0176e635037099
hriday31 <hriday311203@gmail.com>
2023-11-21 13:46:46 +0530

    refactored code for ssd1306

--- espcode/espcode.ino
+++ espcode/espcode.ino
@@ -7,49 +7,38 @@
 #include <ESP32Servo.h>
 #include <Wire.h>
 #include <Adafruit_GFX.h>
-#include <Adafruit_SSD1306.h>
+#include <Adafruit_SH110X.h>
 
-const char* ssid = "A.T.O.M_LABS";
-const char* password = "Atom281121";
+const char* ssid = "A.T.O.M_Labs";
+const char* password = "atom281121";
+//const char* ssid = "HRHK";
+//const char* password = "Ha9868598102@";
+const char* serverIP = "192.168.0.8";
 
-Servo myservo;
-String name;
-WiFiClient client;
-String StringVariable;
 constexpr uint8_t RST_PIN = 5;
 constexpr uint8_t SS_PIN = 26;
+
 MFRC522 mfrc522(SS_PIN, RST_PIN);
 MFRC522::MIFARE_Key key;
 MFRC522::StatusCode status;
+WiFiClient client;
+
 int blockNum = 2;
 byte bufferLen = 18;
 byte readBlockData[18];
 byte blockData[16] = { "Name" };
-int pos = 0;
+String StringVariable;
 String myurl = "/";
 String line = "";
-#define SCREEN_WIDTH 128     // OLED display width, in pixels
-#define SCREEN_HEIGHT 64     // OLED display height, in pixels
-#define OLED_RESET -1        // Reset pin # (or -1 if sharing Arduino reset pin)
-#define SCREEN_ADDRESS 0x3C  ///< See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
-Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
-String IotClientSendWithAnswer(String IPcache, String monmessagecache) {
-  line = "";
-  client.print(String("GET ") + myurl + monmessagecache + " HTTP/1.1\r\n" + "Host: " + IPcache + "\r\n" + "Connection: close\r\n\r\n");
-  unsigned long timeout = millis();
-  while (client.available() == 0) {
-    if (millis() - timeout > 1000) {
-      return "Client Timeout!";
-    }
-  }
-  while (client.available()) { line += client.readStringUntil('\r'); }
-  return line;
-}
+#define i2c_Address 0x3c 
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+#define OLED_RESET -1 
+Adafruit_SH1106G display(128, 64, &Wire, -1);
 
 void setup() {
   Serial.begin(115200);
-  Serial.println("Booting");
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
   while (WiFi.waitForConnectResult() != WL_CONNECTED) {
@@ -58,22 +47,8 @@ void setup() {
     ESP.restart();
   }
 
-  // Port defaults to 3232
-  // ArduinoOTA.setPort(3232);
-
-  // Hostname defaults to esp3232-[MAC]
-  // ArduinoOTA.setHostname("myesp32");
-
-  // No authentication by default
-  // ArduinoOTA.setPassword("admin");
-
-  // Password can be set with it's md5 value as well
-  // MD5(admin) = 21232f297a57a5a743894a0e4a801fc3
-  // ArduinoOTA.setPasswordHash("21232f297a57a5a743894a0e4a801fc3");
-
-  
-
-  ArduinoOTA
+  // Initialize other components here
+   ArduinoOTA
     .onStart([]() {
       String type;
       if (ArduinoOTA.getCommand() == U_FLASH)
@@ -98,56 +73,37 @@ void setup() {
       else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
       else if (error == OTA_END_ERROR) Serial.println("End Failed");
     });
-
   ArduinoOTA.begin();
-
   Serial.println("Ready");
   Serial.print("IP address: ");
   Serial.println(WiFi.localIP());
-
-  pinMode(4, OUTPUT);   //yellow
-  pinMode(2, OUTPUT);   //red
-  pinMode(27, OUTPUT);  //blue
-  pinMode(15, OUTPUT);
-  pinMode(13, OUTPUT); //green
-//  myservo.attach(13);
-
-  StringVariable = "";
-  Serial.begin(115200);
   SPI.begin();
   mfrc522.PCD_Init();
-  if (!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
+  if (!display.begin(i2c_Address, true)) {
     Serial.println(F("SSD1306 allocation failed"));
     for (;;)
       ;
   }
   display.clearDisplay();
-   WiFi.begin("A.T.O.M_LABS", "Atom281121");
-//  WiFi.begin("HRHK", "Ha9868598102@");la  
-  while ((!(WiFi.status() == WL_CONNECTED))) {}
-  Serial.println("ESP code setup");
+  pinMode(13, OUTPUT);
 }
 
 void loop() {
   ArduinoOTA.handle();
-
   StringVariable = "";
   int ch = 0;
-  digitalWrite(27, LOW);
-  digitalWrite(2, LOW);
-  digitalWrite(4, HIGH);
-  digitalWrite(15, LOW);
-//  myservo.write(0);
+  
+  // Your main loop logic here
   digitalWrite(13, LOW);
   display.setTextSize(2);
-  display.setTextColor(SSD1306_WHITE);
+  display.setTextColor(SH110X_WHITE);;
   display.setCursor(25, 0);
   display.println(F("A.T.O.M"));
   display.setCursor(20, 20);
   display.print(F("ROBOTICS"));
-  display.setCursor(0, 40);
-  display.setTextColor(WHITE, BLACK);
-  display.setCursor(0, 40);
+  display.setCursor(0, 45);
+  display.setTextColor(SH110X_WHITE, SH110X_BLACK); 
+  display.setCursor(0, 45);
   display.print(F(" Tap Card "));
   display.display();
   for (byte i = 0; i < 6; i++) {
@@ -159,21 +115,19 @@ void loop() {
   if (!mfrc522.PICC_ReadCardSerial()) {
     return;
   }
-  //  digitalWrite(4,LOW);
-  digitalWrite(15, LOW);
-  ReadDataFromBlock(blockNum, readBlockData);
+   ReadDataFromBlock(blockNum, readBlockData);
   for (int j = 0; j < 16; j++) {
     StringVariable = StringVariable + String((char)readBlockData[j]);
     Serial.write(readBlockData[j]);
   }
-  if (client.connect("192.168.0.25", 8080)) {
-    String m = IotClientSendWithAnswer("192.168.0.25", "Done");
+  if (client.connect(serverIP, 8080)) {
+    String m = IotClientSendWithAnswer(serverIP, "Done");
     if (m != "NULL" && m != "Client Timeout!") {
       m.getBytes(blockData, 16);
-      display.setCursor(0, 40);
-      display.setTextColor(WHITE, BLACK);
-      display.setCursor(0, 40);  // Start at top-left corner
-      display.print(F("  Issuing "));
+      display.setCursor(0, 45);
+        display.setTextColor(SH110X_WHITE, SH110X_BLACK); 
+      display.setCursor(24, 45);  // Start at top-left corner
+      display.print(F("Issuing"));
       display.display();
       delay(500);
       Serial.println("Writing to Data Block...");
@@ -182,31 +136,27 @@ void loop() {
     }
   }
   if (ch != 1) {
-    if (client.connect("192.168.0.25", 8000)) {
-
-      display.setCursor(0, 40);
-      display.setTextColor(WHITE, BLACK);
-      display.setCursor(0, 40);  // Start at top-left corner
-      display.print(F(" Checking "));
+    if (client.connect(serverIP, 8000)) {
+      display.setCursor(0, 45);
+        display.setTextColor(SH110X_WHITE, SH110X_BLACK); 
+      display.setCursor(0, 45);  // Start at top-left corner
+      display.print(F(" Checking"));
       display.display();
-      String m = IotClientSendWithAnswer("192.168.0.25", StringVariable);
+      String m = IotClientSendWithAnswer(serverIP, StringVariable);
       if (m == "Marked") {
-        digitalWrite(15, HIGH);
-//        myservo.write(90);
         digitalWrite(13,HIGH);
-        display.setCursor(0, 40);
-        display.setTextColor(WHITE, BLACK);
-        display.setCursor(0, 40);  // Start at top-left corner
-//        display.print(F("   Member "));
-        display.print(F("oPEN dOOR"));
+        display.setCursor(0, 45);
+          display.setTextColor(SH110X_WHITE, SH110X_BLACK); 
+        display.setCursor(0, 45);  // Start at top-left corner
+//        display.print(F("Member"));
+        display.print(F(" Door Open"));
         display.display();
         delay(500);
       } else if (m == "NotMarked") {
-        digitalWrite(2, HIGH);
-        display.setCursor(0, 40);
-        display.setTextColor(WHITE, BLACK);
-        display.setCursor(0, 40);  // Start at top-left corner
-        display.print(F(" Not Member"));
+        display.setCursor(0, 45);
+          display.setTextColor(SH110X_WHITE, SH110X_BLACK); 
+        display.setCursor(0, 45);  // Start at top-left corner
+        display.print(F("Not Member"));
         display.display();
         delay(500);
       }
@@ -215,11 +165,11 @@ void loop() {
 
   delay(1000);
   mfrc522.PICC_HaltA();
-  mfrc522.PCD_StopCrypto1();
+  mfrc522.PCD_StopCrypto1();// Consider replacing with non-blocking techniques
 }
 
-
 void ReadDataFromBlock(int blockNum, byte readBlockData[]) {
+  // Read data from block
   status = mfrc522.PCD_Authenticate(MFRC522::PICC_CMD_MF_AUTH_KEY_A, blockNum, &key, &(mfrc522.uid));
   if (status != MFRC522::STATUS_OK) {
     return;
@@ -231,6 +181,7 @@ void ReadDataFromBlock(int blockNum, byte readBlockData[]) {
 }
 
 void WriteDataToBlock(int blockNum, byte blockData[]) {
+  // Write data to block
   status = mfrc522.PCD_Authenticate(MFRC522::PICC_CMD_MF_AUTH_KEY_A, blockNum, &key, &(mfrc522.uid));
   if (status != MFRC522::STATUS_OK) {
     Serial.print("Authentication failed for Write: ");
@@ -240,12 +191,27 @@ void WriteDataToBlock(int blockNum, byte blockData[]) {
   if (status != MFRC522::STATUS_OK) {
     return;
   }
-
   display.setCursor(0, 40);
-  display.setTextColor(WHITE, BLACK);
+    display.setTextColor(SH110X_WHITE, SH110X_BLACK); ;
   display.setCursor(0, 40);  // Start at top-left corner
   display.print(F("   Issued "));
   display.display();
   delay(500);
   digitalWrite(27, HIGH);
+}
+
+String IotClientSendWithAnswer(String IPcache,String monmessagecache) {
+  // Send HTTP GET request and return the response
+  line = "";
+  client.print(String("GET ") + myurl + monmessagecache + " HTTP/1.1\r\n" + "Host: " + IPcache + "\r\n" + "Connection: close\r\n\r\n");
+  unsigned long timeout = millis();
+  while (client.available() == 0) {
+    if (millis() - timeout > 1000) {
+      return "Client Timeout!";
+    }
+  }
+  while (client.available()) {
+    line += client.readStringUntil('\r');
+  }
+  return line;
 }
